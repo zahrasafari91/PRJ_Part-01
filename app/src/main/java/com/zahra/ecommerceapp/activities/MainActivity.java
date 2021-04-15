e6faad9ea89a537c1f30995ac063dd3e6c00bf6e
Zahra Safari <safari.zahra.91@gmail.com>
2021-04-15 17:26:29 -0600

    First commit

---
+++ app/src/main/java/com/zahra/ecommerceapp/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.zahra.ecommerceapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zahra.ecommerceapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
